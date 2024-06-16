769e2630b9097a4520a1c140bbd37a0477816e50
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-06-16 10:38:02 +0530

    Handled None Cases

--- backend/src/features/extract_special_features.py
+++ backend/src/features/extract_special_features.py
@@ -1,20 +1,25 @@
 from src.data_processing.special_features import SpecialFeatures
+import src.utils.constants as constants
 
 def get_special_features(domain)->list:
-    special_features = SpecialFeatures(domain)
-    return [
-        special_features.time_response(),
-        special_features.domain_spf(),
-        special_features.asn_ip(),
-        special_features.time_domain_activation(),
-        special_features.time_domain_expiration(),
-        special_features.qty_ip_resolved(),
-        special_features.qty_nameservers(),
-        special_features.qty_mx_servers(),
-        special_features.ttl_hostname(),
-        special_features.tls_ssl_certificate(),
-        special_features.qty_redirects(),
-        special_features.url_google_index(),
-        special_features.domain_google_index(),
-        special_features.url_shortened(),
-    ]
+    if domain is not None:
+        print(domain)
+        special_features = SpecialFeatures(domain)
+        return [
+            special_features.time_response(),
+            special_features.domain_spf(),
+            special_features.asn_ip(),
+            special_features.time_domain_activation(),
+            special_features.time_domain_expiration(),
+            special_features.qty_ip_resolved(),
+            special_features.qty_nameservers(),
+            special_features.qty_mx_servers(),
+            special_features.ttl_hostname(),
+            special_features.tls_ssl_certificate(),
+            special_features.qty_redirects(),
+            special_features.url_google_index(),
+            special_features.domain_google_index(),
+            special_features.url_shortened(),
+        ]
+    else:
+        return constants.NOT_FOUND_CONSTANT * constants.SPECIAL_FEATURES
