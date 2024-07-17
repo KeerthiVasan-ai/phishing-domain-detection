e1c1ef428005332e70e7299101542e47990e9834
KeerthiVasan-ai <keerthi77459@gmail.com>
2024-07-17 16:24:47 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DetectorUI from './components/DetectorUI/DetectorUI';
+
+function App() {
+  return (
+    <div >
+      <DetectorUI />
+    </div>
+  );
+}
+
+export default App;
